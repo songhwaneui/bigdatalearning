230188af3d9cdf1450204194eaae61dd68d5bdcb
SeoDongWook <http94@naver.com>
2019-07-24 11:31:19 +0900

    자료추가
    Signed-off-by: SeoDongWook <http94@naver.com>

---
+++ 서동욱/수업_1주차/필기/star.py
@@ -0,0 +1,36 @@
+﻿#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a = 1
+_b = 2
+
+
+# In[3]:
+
+
+def x():
+    return 1
+
+
+# In[4]:
+
+
+def _y():
+    return 2
+
+
+# In[6]:
+
+
+__all__ = '_y()'
+
+
+
+# In[7]:
+
+
+c = '문근영'
+
