bb3751a7531316d80fd83ac016ea903637edbe23
eunha <1002galaxy@gmail.com>
2019-07-26 17:43:29 +0900

    190726

---
+++ 김은하/1주차/star.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+a = 1
+
+
+# In[8]:
+
+
+_b = 2
+
+
+# In[ ]:
+
+
+c = '문근영'
+
+
+# In[3]:
+
+
+def x():
+    return 1
+
+
+# In[5]:
+
+
+def _y():
+    return 2
+
+
+# In[ ]:
+
+
+__all__ = ['a']
+
